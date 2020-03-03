5fd35d7c5847b59252fe710ea5f24badd1ec4d61
MartinL3dl <martin.ledl@student.ossp.cz>
2020-03-03 08:04:38 +0100

    Create wp-01-03.php
    dodělávka mil. hodina

---
+++ wp-01-03.php
@@ -0,0 +1,23 @@
+<?php
+$x ="15";
+$y ="5";
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <p>a= <?=$x ?></p>
+   <p>b = <?=$y ?></p>
+   <p>součet = <?=$x?>+<?=$y?>=<?=$x+$y?></p>
+   <p>rozdíl = <?=$x?>-<?=$y?>=<?=$x-$y?></p>
+   <p>součin = <?=$x?>X<?=$y?>=<?=$x*$y?></p>
+   <p>podíl = <?=$x?>:<?=$y?>=<?=$x/$y?></p>
+</body>
+</html>
